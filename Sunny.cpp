4d13c4dcc487e73a8117f816f12a53e36a0d2d91
StelleTbz <3542578897@qq.com>
2026-02-27 07:17:46 +0800

    modified:   Sunny.cpp

--- Sunny.cpp
+++ Sunny.cpp
@@ -1,22 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-//consts
-//values
-//fundef
+// consts
+// values
+// fundef
 bool jdg(int);
-//main
-int main(){
-    int n,cnt=0,sum=0;
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        cnt+=i;
-        sum+=cnt;
+// main
+int main()
+{
+    int n, cnt = 0, sum = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cnt += i;
+        sum += cnt;
     }
-    cout<<sum;
+    cout << sum;
     return 0;
 }
-//funcdo 
-bool jdg(int n){
-    if(n%100==0) n/=100;
-    return n%4==0;
+// funcdo
+bool jdg(int n)
+{
+    if (n % 100 == 0)
+        n /= 100;
+    return n % 4 == 0;
 }
