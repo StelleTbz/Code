1ca0f73f45b2590108c7af58bdd57b0c21f87679
StelleTbz <3542578897@qq.com>
2026-02-26 10:15:13 +0800

    modified:   Sunny.cpp

--- Sunny.cpp
+++ Sunny.cpp
@@ -3,11 +3,21 @@ using namespace std;
 //consts
 //values
 //fundef
+bool jdg(int);
 //main
 int main(){
-    int bg,ed;
+    int bg,ed,sum=0;
     cin>>bg>>ed;
-    
+    for(int i=bg+1;i<ed;i++){
+        if(jdg(i)) {
+            sum+=i;
+        }
+    }
+    cout<<sum;
     return 0;
 }
 //funcdo 
+bool jdg(int n){
+    if(n%100==0) n/=100;
+    return n%4==0;
+}
