7902dc1a9f6fe874fb044bddd6222c11a760aad8
StelleTbz <3542578897@qq.com>
2026-02-28 08:02:09 +0800

    modified:   Sunny.cpp

--- Sunny.cpp
+++ Sunny.cpp
@@ -9,7 +9,7 @@ int main(){
     int h,s,m,dt,t;
     cin>>h>>m>>s>>dt;
     t=dt+s+60*m+3600*h;
-    cout<<t%3600<<" "<<t/3600%60<<" "<<t%60;
+    cout<<t/3600<<" "<<t%3600/60<<" "<<t%60;
     pus();
     return 0;
 }
